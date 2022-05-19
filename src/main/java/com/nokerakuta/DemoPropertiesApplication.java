8cf0e6663ca28d48f634108b936ec1153355eea9
mEllvin_work <me1vin@mail.ru>
2022-05-19 10:58:06 +0400

    useless annotation remove

--- src/main/java/com/nokerakuta/DemoPropertiesApplication.java
+++ src/main/java/com/nokerakuta/DemoPropertiesApplication.java
@@ -1,16 +1,13 @@
 package com.nokerakuta;
 
-import com.nokerakuta.config.TestProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(TestProperties.class)
 public class DemoPropertiesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoPropertiesApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoPropertiesApplication.class, args);
+  }
 
 }
